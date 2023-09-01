465822e731ab1a969c51ce9159c15a95c56fc9fb
Kev1nJ <kevinjerome85@yahoo.com>
2023-08-31 22:27:37 -0400

    README Generator, needs template production

---
+++ README.md
@@ -0,0 +1,18 @@
+# ReadMe Generator
+by Kevin Jerome
+
+## Table of Contents
+* [Description](#description)
+* [Contact-Me](#contact-me)
+* [Demonstration](#demonstration)
+
+## Description
+- The purpose of this project is to generator a set template of questions that any user can answer to create a quality README file. The aim for this is to cut done on time for creating README files to be committed to Github repos and make the task of creating a README file simpler and more effecient. (this project is incomplete , the full functionality of the README generator is inefficient)
+
+## Contact-Me
+* Name - Kevin Jerome
+* Email - kevinjerome85@yahoo.com
+* Github - https://github.com/Kev1nJ
+
+## Demonstration
+* README video: https://drive.google.com/file/d/1Y4C5sEljueaPTF1GMcZM5MwIqSKcXtl8/view
---
+++ index.js
@@ -0,0 +1,53 @@
+// TODO: Include packages needed for this application
+
+const inquirer = require("inquirer")
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Please name your Project.",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please describe the purpose and functionality of this project.",
+  },
+  {
+    type: "input",
+    name: "creator",
+    message: "Write your GitHub username.",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Provide a valid email address.",
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "Please list any contributors. (Use GitHub usernames)",
+    default: "",
+  },
+  {
+    type: "input",
+    name: "features",
+    message: "What features makes your project stand out?.",
+  },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((responses) => {
+    console.log("Creating quality README.md File...");
+    // console.log(JSON.stringify(answers, null, '  '));
+
+})
+}
+
+// Function call to initialize app
+init();
